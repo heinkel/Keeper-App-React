dd8539819abd68123277996bbba69c7fbfb7fd97
heinkel <juan.ortiz.tandil@gmail.com>
2020-08-10 14:09:01 +0900

    auto loading data cards from notes element list

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import ReactDom from "react-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "./notes"
+
 
 function App(){
+    
     return <div> 
                 <Header />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
-                <Note />
+                {notes.map(note => {
+                    return(
+                        <Note 
+                            key={note.key}
+                            title={note.title}
+                            content={note.content}
+                        />
+                    )
+                })}
                 <Footer />
             </div>;
 }
--- src/Note.jsx
+++ src/Note.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
-function Note(){
+function Note(props){
  
-    return <div class="note">
-                <h1>Titulo de mi Nota</h1>
-                <p> Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information</p>
+    return <div className="note">
+                <h1>{props.title}</h1>
+                <p> {props.content}</p>
            </div>;
 }
 
